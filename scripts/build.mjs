b86cee08a1a3d4b737e7e726f755ac0acc06ec96
Sidath <sidathasiri@gmail.com>
2025-06-09 17:19:04 +0530

    fix: fix the issue in the deploy script

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,19 +1,2 @@
 #!/usr/bin/env zx
-import path from 'path'
-
-const env = "DEV"
-
-// Read the environment variable values from the JSON file
-const envVarsPath = path.join(__dirname, `../configurations/${env}.config.json`)
-
-// Set up env variables
-const envVarsData = require(envVarsPath)
-for (const key in envVarsData) {
-    if (envVarsData.hasOwnProperty(key)) {
-        const value = envVarsData[key]
-        if (key && value) {
-            process.env[key.trim()] = value.trim()
-        }
-    }
-}
 await $`npm run build-storybook`;
