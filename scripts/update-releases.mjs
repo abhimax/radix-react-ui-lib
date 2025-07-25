95f475512bd51ba8e00d5c6b447c4bbea2455262
Sidath <sidathasiri@gmail.com>
2025-07-25 10:45:53 +0530

    update script

--- scripts/update-releases.mjs
+++ scripts/update-releases.mjs
@@ -7,7 +7,7 @@ const releasesFilePath = path.join(__dirname, '../releases-page/releases.json');
 
 async function fetchReleasesFromGitHub() {
     try {
-        const result = await $`gh release list --repo abhimax/radix-react-ui-lib --json tagName,publishedAt --jq ".[] | {tag_name: .tagName, publishedAt: .publishedAt}"`;
+        const result = await $`gh release list --repo abhimax/radix-react-ui-lib --json name,publishedAt --jq ".[] | {name: .name, publishedAt: .publishedAt}"`;
         console.log('Raw output from gh CLI:', result);
         console.log('Raw output from gh CLI stdout:', result.stdout);
 
